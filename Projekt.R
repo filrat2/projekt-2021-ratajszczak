d86f8a292d1db2b7e1050e1a06457f42ddee4e4a
Jan-Mal <jan.zb.malak@gmail.com>
2021-04-07 23:28:19 +0200

    szkic projektu

--- Projekt.R
+++ Projekt.R
@@ -16,3 +16,26 @@ names(kursy)[names(kursy) == "Nazwa.waluty"] = "nazwa_waluty"
 colnames(kursy)
 kursy
 
+
+EUR = kursy$kurs_waluty[8]
+
+zakup = function(x, y){
+  x / y  
+}
+zakup(100, EUR) #jeśli zapłacisz x zł, dostaniesz wynik w wybranej walucie
+
+
+sprzedaz = function(x, y){
+  x * y  
+}
+sprzedaz(100, EUR) #jeśli sprzedasz x danej waluty, dostaniesz wynik w zł
+
+
+
+
+x = 3
+kursy$symbol_waluty[x]
+kursy$kurs_waluty[x]
+k = 1000 / kursy$kurs_waluty[x]
+k
+
