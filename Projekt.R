d65516e4d4cbe9a48e03812efdb3c7ee3892988c
Jan-Mal <jan.zb.malak@gmail.com>
2021-04-07 22:50:55 +0200

    zamiana pliku excel na ramkę danych

--- Projekt.R
+++ Projekt.R
@@ -2,7 +2,17 @@ install.packages("readxl")
 
 library(readxl)
 
-kursy = read_excel("kursy.xlsx")
+kursy_plik = read_excel("kursy.xlsx")
 
-print(kursy, n=nrow(kursy))
+print(kursy_plik, n=nrow(kursy_plik))
+
+kursy = data.frame(kursy_plik)
+kursy
+
+colnames(kursy)
+names(kursy)[names(kursy) == "Symbol.waluty"] = "symbol_waluty"
+names(kursy)[names(kursy) == "Kurs.waluty"] = "kurs_waluty"
+names(kursy)[names(kursy) == "Nazwa.waluty"] = "nazwa_waluty"
+colnames(kursy)
+kursy
 
