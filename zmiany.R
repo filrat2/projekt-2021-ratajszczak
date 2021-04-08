ffe418abcd80c13effdf7564da757fa00928c619
Arkadi293 <Arkadi293@o2.pl>
2021-04-08 14:16:25 +0200

    Dodałem nową funkcję "Jaka_to_waluta?".

--- zmiany.R
+++ zmiany.R
@@ -18,3 +18,108 @@ sprzedaz = function(x, y){
 }
 sprzedaz(100, EUR) 
 
+#POPRAWA
+
+kupno = function(x, y){
+  
+  if (x / y){
+    cat(paste("Za", x, "zł, możesz wymienić", (x / y), 
+              "wybranej waluty."))
+  }
+}
+
+kupno(130, EUR) 
+
+
+sprzedaz = function(x, y){
+  
+  if (x * y){
+    cat(paste("Jeśli sprzedasz",x ,y %in% kursy , "otrzymasz",
+              (x * y), "zł."))
+  } 
+}
+sprzedaz(130, EUR) 
+
+
+
+
+
+
+
+
+#FUNKCJA_JAKA_TO_WALUTA?
+
+jaka_to_waluta = function(kraj){
+  
+  kraj
+  
+}
+
+jaka_to_waluta(Rumunia)
+
+
+kraj = c(Tajlandia, StanyZjednoczone, Australia, Hongkong, Kanada, NowaZelandia,
+         Singapur, EUR, Węgry, Szwajcaria, WielkaBrytania, Ukraina, Japonia,
+         Czechy, Dania, Islandia, Norwegia, Szwecja, Chorwacja, Rumunia,
+         Bułgaria, Turcja, Izrael, Chile, Filipiny, Meksyk, RPA, Brazylia,
+         Malezja, Rosja, Indonezja, Indie, KoreaPołudniowa, Chiny, XDR)
+
+Tajlandia = c("bat tajski", "THB")  
+StanyZjednoczone = c("dolar amerykański", "USD")
+Australia = c("dolar australijski", "AUD")
+Hongkong = c("dolar Hongkongu", "HKD")
+Kanada = c("dolar kanadyjski", "CAD")
+NowaZelandia = c("dolar nowozelandzki", "NZD")
+Singapur = c("dolar singapurski", "SGD")
+Niemcy = c("euro", "EUR")
+Hiszpania = c("euro", "EUR")
+Słowacja = c("euro", "EUR")
+Włochy = c("euro", "EUR")
+Francja = c("euro", "EUR")
+Finlandia = c("euro", "EUR")
+Czarnogóra = c("euro", "EUR")
+Austria = c("euro", "EUR")
+Portugalia = c("euro", "EUR")
+Malta = c("euro", "EUR")
+Litwa = c("euro", "EUR")
+Estonia = c("euro", "EUR")
+Andora = c("euro", "EUR")
+Łotwa = c("euro", "EUR")
+Belgia = c("euro", "EUR")
+Cypr = c("euro", "EUR")
+Słowenia = c("euro", "EUR")
+Luksemburg = c("euro", "EUR")
+Holandia = c("euro", "EUR")
+Irlandia = c("euro", "EUR")
+Monako = c("euro", "EUR")
+SanMarino = c("euro", "EUR")
+Watykan = c("euro", "EUR")
+Grecja = c("euro", "EUR")
+Węgry = c("forint", "HUF")  
+Szwajcaria = c("frank szwajcarski", "CHF")
+WielkaBrytania = c("funt szterling", "GBP")
+Ukraina = c("hrywna (Ukraina)", "UAH")
+Japonia = c("jen", "JPY")
+Czechy = c("korona czeska","CZK")
+Dania = c("korona duńska", "DKK")
+Islandia = c("korona islandzka", "ISK")
+Norwegia = c("korona norweska", "NOK")
+Szwecja = c("korona szwedzka", "SEK")
+Chorwacja =  c("kuna chorwacka", "HRK")
+Rumunia = c("lej rumuński", "RON")
+Bułgaria = c("lew (Bułgaria)", "BGN")
+Turcja = c("lira turecka", "TRY")
+Izrael = c("nowy izraelski szekel", "ILS")
+Chile = c("peso chilijskie", "CLP")
+Filipiny = c("peso filipińskie", "PHP")
+Meksyk = c("peso meksykańskie", "MXN")
+RPA = c("rand", "ZAR")
+Brazylia = c("real (Brazylia)", "BRL")
+Malezja = c("ringgit (Malezja)", "MYR")
+Rosja = c("rubel rosyjski", "RUB")
+Indonezja = c("rupia indonezyjska", "IDR")
+Indie = c("rupia indyjska", "INR")
+KoreaPołudniowa = c("won południowokoreański", "KRW")
+Chiny = c("yuan renminb", "CNY")
+XDR = c("MFW", "SDR")
+
