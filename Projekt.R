6b81d7b8dd1cfde85320317ffe6ab2423ce417ab
Arkadi293 <Arkadi293@o2.pl>
2021-04-08 11:53:21 +0200

    Przesunięcie funkcji porownanie do lewej krawedzi.

--- Projekt.R
+++ Projekt.R
@@ -85,3 +85,24 @@ kursy$kurs_waluty[x]
 k = 1000 / kursy$kurs_waluty[x]
 k
 
+kupno = function(x, y){
+  
+  if (x / y){
+    cat(paste("Jeśli zapłacisz", x, "zł,","dostaniesz" , (x / y), 
+              "wartości waluty."))
+  }
+}
+
+kupno(130, EUR) 
+
+
+sprzedaz = function(x, y){
+  
+  if (x * y){
+    cat(paste("Jeśli sprzedasz liczbę",x ,"wartosci waluty,","dostaniesz",
+              (x * y), "zł." ))
+  } 
+}
+sprzedaz(100, EUR) 
+
+
