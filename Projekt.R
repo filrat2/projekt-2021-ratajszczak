0a67f6314a3a9792255a64898a173b3eda39f54e
Jan-Mal <jan.zb.malak@gmail.com>
2021-04-08 20:15:51 +0200

    poprawa krajów

--- Projekt.R
+++ Projekt.R
@@ -1,5 +1,3 @@
-install.packages("readxl")
-
 library(readxl)
 
   #stworzenie ramki danych z kursami walut
@@ -17,6 +15,7 @@ names(kursy)[names(kursy) == "Kurs.waluty"] = "kurs_waluty"
 names(kursy)[names(kursy) == "Nazwa.waluty"] = "nazwa_waluty"
 colnames(kursy)
 kursy
+kursy[3,]
 
   #przypisanie kursów do międzynarodowych symboli walut
 
@@ -77,79 +76,73 @@ sprzedaz(130, EUR)
   
   #Funkcja jaka_to_waluta
   
-jaka_to_waluta = function(kraj){
-  kraj
-  }
-  
-kraj = c(Tajlandia, StanyZjednoczone, Australia, Hongkong, Kanada, NowaZelandia,
-         Singapur, EUR, Węgry, Szwajcaria, WielkaBrytania, Ukraina, Japonia,
-         Czechy, Dania, Islandia, Norwegia, Szwecja, Chorwacja, Rumunia,
-         Bułgaria, Turcja, Izrael, Chile, Filipiny, Meksyk, RPA, Brazylia,
-         Malezja, Rosja, Indonezja, Indie, KoreaPołudniowa, Chiny, XDR)
-  
-Tajlandia = c("bat tajski", "THB")  
-StanyZjednoczone = c("dolar amerykański", "USD")
-Australia = c("dolar australijski", "AUD")
-Hongkong = c("dolar Hongkongu", "HKD")
-Kanada = c("dolar kanadyjski", "CAD")
-NowaZelandia = c("dolar nowozelandzki", "NZD")
-Singapur = c("dolar singapurski", "SGD")
-Niemcy = c("euro", "EUR")
-Hiszpania = c("euro", "EUR")
-Slowacja = c("euro", "EUR")
-Wlochy = c("euro", "EUR")
-Francja = c("euro", "EUR")
-Finlandia = c("euro", "EUR")
-Czarnogora = c("euro", "EUR")
-Austria = c("euro", "EUR")
-Portugalia = c("euro", "EUR")
-Malta = c("euro", "EUR")
-Litwa = c("euro", "EUR")
-Estonia = c("euro", "EUR")
-Andora = c("euro", "EUR")
-Lotwa = c("euro", "EUR")
-Belgia = c("euro", "EUR")
-Cypr = c("euro", "EUR")
-Slowenia = c("euro", "EUR")
-Luksemburg = c("euro", "EUR")
-Holandia = c("euro", "EUR")
-Irlandia = c("euro", "EUR")
-Monako = c("euro", "EUR")
-SanMarino = c("euro", "EUR")
-Watykan = c("euro", "EUR")
-Grecja = c("euro", "EUR")
-Wegry = c("forint", "HUF")  
-Szwajcaria = c("frank szwajcarski", "CHF")
-WielkaBrytania = c("funt szterling", "GBP")
-Ukraina = c("hrywna (Ukraina)", "UAH")
-Japonia = c("jen", "JPY")
-Czechy = c("korona czeska","CZK")
-Dania = c("korona duńska", "DKK")
-Islandia = c("korona islandzka", "ISK")
-Norwegia = c("korona norweska", "NOK")
-Szwecja = c("korona szwedzka", "SEK")
-Chorwacja =  c("kuna chorwacka", "HRK")
-Rumunia = c("lej rumuński", "RON")
-Bulgaria = c("lew (Bułgaria)", "BGN")
-Turcja = c("lira turecka", "TRY")
-Izrael = c("nowy izraelski szekel", "ILS")
-Chile = c("peso chilijskie", "CLP")
-Filipiny = c("peso filipińskie", "PHP")
-Meksyk = c("peso meksykańskie", "MXN")
-RPA = c("rand", "ZAR")
-Brazylia = c("real (Brazylia)", "BRL")
-Malezja = c("ringgit (Malezja)", "MYR")
-Rosja = c("rubel rosyjski", "RUB")
-Indonezja = c("rupia indonezyjska", "IDR")
-Indie = c("rupia indyjska", "INR")
-KoreaPoludniowa = c("won południowokoreański", "KRW")
-Chiny = c("yuan renminb", "CNY")
-XDR = c("MFW", "SDR")
-Polska = c("polski złoty", "PLN")
 
-jaka_to_waluta(Rumunia)
+kraje = print("Obsługiwane przez nasz zbiór funkcji państwa to: Tajlandia, Stany Zjednoczone, Australia, Hongkong, Kanada, Nowa Zelandia, Singapur, Niemcy, Hiszpania, Słowacja, Włochy, Francja, Czarnogóra, Austria, Portugalia, Malta, Litwa, Estonia, Andora, Łotwa, Belgia, Cypr, Słowenia, Luksemburg, Holandia, Irlandia, Monako, San Marino, Watykan, Greja, Węgry, Szwajcaria, Wielka Brytania, Ukraina, Japonia, Czechy, Dania, Islandia, Norwegia, Szwecja, Chorwacja, Rumunia, Bułgaria, Turcja, Izrael, Chile, Filipiny, Meksyk, RPA, Brazylia, Malezja, Rosja, Indonezja, Indie, Korea Południowa, Chiny, Polska")
 
-jaka_to_waluta(Polska)
+kraje 
+
+Tajlandia = kursy[1,]  
+StanyZjednoczone = kursy[2,]
+Australia = kursy[3,]
+Hongkong = kursy[4,]
+Kanada = kursy[5,]
+NowaZelandia = kursy[6,]
+Singapur = kursy[7,]
+Niemcy = kursy[8,]
+Hiszpania = kursy[8,]
+Slowacja = kursy[8,]
+Wlochy = kursy[8,]
+Francja = kursy[8,]
+Finlandia = kursy[8,]
+Czarnogora = kursy[8,]
+Austria = kursy[8,]
+Portugalia = kursy[8,]
+Malta = kursy[8,]
+Litwa = kursy[8,]
+Estonia = kursy[8,]
+Andora = kursy[8,]
+Lotwa = kursy[8,]
+Belgia = kursy[8,]
+Cypr = kursy[8,]
+Slowenia = kursy[8,]
+Luksemburg = kursy[8,]
+Holandia = kursy[8,]
+Irlandia = kursy[8,]
+Monako = kursy[8,]
+SanMarino = kursy[8,]
+Watykan = kursy[8,]
+Grecja = kursy[8,]
+Wegry = kursy[9,]  
+Szwajcaria =  kursy[10,]
+WielkaBrytania =  kursy[11,]
+Ukraina =  kursy[12,]
+Japonia =  kursy[13,]
+Czechy =  kursy[14,]
+Dania =  kursy[15,]
+Islandia =  kursy[16,]
+Norwegia =  kursy[17,]
+Szwecja =  kursy[18,]
+Chorwacja =   kursy[19,]
+Rumunia =  kursy[20,]
+Bulgaria =  kursy[21,]
+Turcja =  kursy[22,]
+Izrael =  kursy[23,]
+Chile =  kursy[24,]
+Filipiny =  kursy[25,]
+Meksyk = kursy[26,]
+RPA =  kursy[27,]
+Brazylia =  kursy[28,]
+Malezja =  kursy[29,]
+Rosja = kursy[30,]
+Indonezja =  kursy[31,]
+Indie =  kursy[32,]
+KoreaPoludniowa =  kursy[33,]
+Chiny = kursy[34,]
+XDR =  kursy[35,]
+Polska = data.frame(symbol_waluty = "PLN",
+      kurs_waluty = "1.00",
+      nazwa_waluty = "polski złoty")
+
 
   #Funkcja porownanie
   
