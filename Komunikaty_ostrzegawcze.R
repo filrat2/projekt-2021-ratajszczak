5609cf2844169190e280db37b8923c5687b13aa1
Arkadi293 <Arkadi293@o2.pl>
2021-04-09 19:40:09 +0200

    AKTUALIZACJA: szkic funkcji

--- Komunikaty_ostrzegawcze.R
+++ Komunikaty_ostrzegawcze.R
@@ -1,5 +1,5 @@
-sym_wal = data.frame(x = (c("PLN, USD, AUD, HKD, CAD, NZD, SGD, EUR, HUF, CHF, GBP, UAH, JPY, CZK, DKK, ISK, NOK, SEK, HRK, RON, BGN, TRY, ILS, CLP, PHP, MXN, ZAR, BRL, MYR, IDR, INR, KRW, CNY, XDR")),
-                     y = (c("PLN, USD, AUD, HKD, CAD, NZD, SGD, EUR, HUF, CHF, GBP, UAH, JPY, CZK, DKK, ISK, NOK, SEK, HRK, RON, BGN, TRY, ILS, CLP, PHP, MXN, ZAR, BRL, MYR, IDR, INR, KRW, CNY, XDR")))
+sym_wal = data.frame(x = cbind(c("PLN, USD, AUD, HKD, CAD, NZD, SGD, EUR, HUF, CHF, GBP, UAH, JPY, CZK, DKK, ISK, NOK, SEK, HRK, RON, BGN, TRY, ILS, CLP, PHP, MXN, ZAR, BRL, MYR, IDR, INR, KRW, CNY, XDR")),
+                     y = cbind(c("PLN, USD, AUD, HKD, CAD, NZD, SGD, EUR, HUF, CHF, GBP, UAH, JPY, CZK, DKK, ISK, NOK, SEK, HRK, RON, BGN, TRY, ILS, CLP, PHP, MXN, ZAR, BRL, MYR, IDR, INR, KRW, CNY, XDR")))
 sym_wal
 
 
@@ -11,18 +11,29 @@ kalkulator_walut = function(x, y){
   vals = c(x, y)
   
   if (is.numeric(vals)){
-  stop("ERROR | Wszystkie argumenty muszą zawierać znaki!")
+  stop("ERROR! | Wszystkie argumenty muszą zawierać znaki.")
   
   }
 
-  if (!all(c(length(x), length(y)) == 3)){
-  stop("ERROR! | Każdemu argumentowi przypisane są tylko trzy wartości.")
-
+  if (!all(c(length(x), length(y)) == 1)){
+  stop("ERROR! | Każdemu argumentowi przypisane jest tylko jedna wartość.")
 
   }     
+  
+  if ((c(x[1], y[1]) == c(x[2], y[2]))){
+  stop("ERROR! | Argumenty nie mogą się powtarzać.")
+  
+  }
+  
+  sym_wal = data.frame(x, y)
+  structure(sym_wal, class = "waluta" )
+  
 }
 
+kalkulator_walut(HRK, EUR)
 
+
+   
   
   
   
