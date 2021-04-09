79dfbed3b08ef56ebb238891b2fe97217b453e33
Arkadi293 <Arkadi293@o2.pl>
2021-04-09 18:00:42 +0200

    AKTUALIZACJA: Utworzenie nowego wektora zawierającego ramkę danych z symbolami walut.

---
+++ Komunikaty_ostrzegawcze.R
@@ -0,0 +1,4 @@
+sym_wal = data.frame(x = (c("PLN, USD, AUD, HKD, CAD, NZD, SGD, EUR, HUF, CHF, GBP, UAH, JPY, CZK, DKK, ISK, NOK, SEK, HRK, RON, BGN, TRY, ILS, CLP, PHP, MXN, ZAR, BRL, MYR, IDR, INR, KRW, CNY, XDR")),
+                     y = (c("PLN, USD, AUD, HKD, CAD, NZD, SGD, EUR, HUF, CHF, GBP, UAH, JPY, CZK, DKK, ISK, NOK, SEK, HRK, RON, BGN, TRY, ILS, CLP, PHP, MXN, ZAR, BRL, MYR, IDR, INR, KRW, CNY, XDR")))
+
+kalk
