a6705ceb77c967a3d226f90a4640c3f58dc430a2
Arkadi293 <Arkadi293@o2.pl>
2021-04-10 23:43:12 +0200

    AKTUALIZACJA: dodanie funkcji segregacji średnich kursów walut.

---
+++ zmiany.R
@@ -0,0 +1,32 @@
+# FUNKCJA_SEGREGACJI_WARTOŚCI_WALUT
+
+# Dzięki tej funkcji użytkownik jest w stanie sprawdzić średni zakres wartości walut.
+
+# seg_wal - skrót od segregacji walut 
+
+seg_wal = data.frame(kursy[1], kursy[2], round((kursy[3]), digits = 4))
+seg_wal
+
+which(seg_wal$kurs_waluty > 1.00)
+
+which(seg_wal$kurs_waluty < 1.00)
+
+which(seg_wal$kurs_waluty > 2.00)
+
+which(seg_wal$kurs_waluty > 3.00)
+
+which(seg_wal$kurs_waluty > 4.00)
+
+which(seg_wal$kurs_waluty > 5.00)
+
+# W dalszym kroku użytkownik ma możliwość sprawdzenia aktualnego najwyższego i najniższego średniego kursu walut.  
+
+wartosc_waluty = data.frame(THB, USD, AUD, HKD, CAD, NZD, SGD, EUR, HUF, CHF, GBP, UAH, JPY, CZK, DKK, ISK, NOK, SEK, HRK, RON, BGN, TRY, ILS, CLP, PHP, MXN, ZAR, BRL, MYR, RUB, IDR, INR, KRW, CNY, XDR)
+wartosc_waluty
+
+seg_wal
+
+which.max(wartosc_waluty)
+
+which.min(wartosc_waluty)
+
